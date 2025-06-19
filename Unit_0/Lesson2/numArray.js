184b76d7da3b2fc5a86febdc8dcf0ffba81837df
Bubbles <sediiskaki@gmail.com>
2025-06-19 18:41:13 +0200

    Delete Unit_0 directory

--- Unit_0/Lesson2/numArray.js
+++
@@ -1,4 +0,0 @@
-let printNumbers = arr => {
-arr.forEach(num => console.log(num));
-};
-printNumbers([1,5,15,10,25]);
--- Unit_0/Lesson2/printArray.js
+++
@@ -1,4 +0,0 @@
-let printNumbers = arr => {
-arr.forEach(num => console.log(num));
-};
-printNumbers([12,52,1,7,5]);
